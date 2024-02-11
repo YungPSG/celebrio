a865d3f1612ab1ad2690288adf13c24478a892a7
YungPSG <jjohnson0994@gmail.com>
2024-02-10 21:46:00 -0600

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  function callTestAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {callTestAPI()}
         </p>
         <a
           className="App-link"
